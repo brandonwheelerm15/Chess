755f82348470b005fb4bf5981b4f51ec5d5d0ff9
Zack <43100488+Zstehura@users.noreply.github.com>
2018-10-16 22:58:31 -0400

    Create ai.cpp

---
+++ Chess/Code/AI/ai.cpp
@@ -0,0 +1,23 @@
+// AI Player instances
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+using namespace WinChess;
+
+void SaveAI(ofstream &oin, int* stat) {
+
+}
+
+int* LoadAI(ifstream fin) {
+
+}
+
+int* getPossibleMoves(Board b, AI) {
+	vector<vector<int>> moves;
+}
+
+void MakeMove(AI comp, Board &b) {
+
+}
+
